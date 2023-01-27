534e6df6547ae86ad3901b741ca27d4c4852dba8
razustha <razustha@hotmail.com>
2023-01-27 09:57:51 +0545

    edit on employe edit blade

---
+++ database/migrations/2023_01_27_035303_create_tbl_employee_table.php
@@ -0,0 +1,66 @@
+<?php
+
+        use Illuminate\Database\Migrations\Migration;
+
+        use Illuminate\Database\Schema\Blueprint;
+
+        use Illuminate\Support\Facades\Schema;
+
+        return new class extends Migration
+
+        {
+
+            public function up()
+
+            {
+Schema::create("tbl_employee", function (Blueprint $table) {
+$table->integer("employee_id")->autoIncrement();
+$table->integer("employeeType_id")->default(0);
+$table->string("firstName")->nullable();
+$table->string("middleName")->nullable();
+$table->string("lastName")->nullable();
+$table->integer("gender_id")->default(0);
+$table->string("nationality_id")->nullable();
+$table->string("password")->nullable();
+$table->string("temproryAddress")->nullable();
+$table->string("phoneNumber")->nullable();
+$table->string("alternateNumber")->nullable();
+$table->string("emailAddress")->nullable();
+$table->integer("country_id")->default(0);
+$table->integer("city_id")->default(0);
+$table->integer("state_id")->default(0);
+$table->integer("district_id")->default(0);
+$table->string("permanentAddress")->nullable();
+$table->integer("postalCode")->default(0);
+$table->integer("organization_id")->default(0);
+$table->integer("department_id")->default(0);
+$table->string("designation_id")->nullable();
+$table->string("reportingTo")->nullable();
+$table->string("skills")->nullable();
+$table->integer("panNo")->default(0);
+$table->integer("document_id")->default(0);
+$table->string("documentName")->nullable();
+$table->string("documentPath")->nullable();
+$table->string("profilePhoto")->nullable();
+$table->text("aboutMe")->nullable();;
+$table->string("sign")->nullable();
+$table->string("fatherName")->nullable();
+$table->string("motherName")->nullable();
+$table->string("grandFatherName")->nullable();
+$table->string("grandMotherName")->nullable();
+$table->string("spouse")->nullable();
+$table->string("emergencyContact")->nullable();
+$table->datetime("createdOn")->nullable();;
+$table->string("createdBy")->nullable();
+$table->integer("updatedBy")->default(0);
+$table->string("alias")->nullable();
+$table->integer("status")->default(0);
+$table->text("remarks")->nullable();;
+$table->timestamps();
+ });
+   }
+    public function down()
+    {
+     Schema::dropIfExists("tbl_employee");
+    }
+};
