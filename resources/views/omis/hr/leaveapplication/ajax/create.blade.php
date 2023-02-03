971396cb48ae81d05f65cd26c373c21f27f3a21e
razustha <razustha@hotmail.com>
2023-02-03 18:12:49 +0545

    edit label and field

--- resources/views/omis/hr/leaveapplication/ajax/create.blade.php
+++ resources/views/omis/hr/leaveapplication/ajax/create.blade.php
@@ -6,26 +6,25 @@
 
         {{ createHidden('employee_id', 'employee_id', '', 'employee_id', auth()->user()->id) }}
         <div class="col-lg-4">
-            <label  class="form-label">Leave Type</label>
+            <label class="form-label">Leave Type</label>
             <div class="input-group">
-            {{ customCreateSelect('leaveType', 'leaveType', '', '', ['sick' => 'Sick', 'paid' => 'Paid']) }}
-            <button class="btn btn-outline-light" type="button" data-bs-toggle="modal" data-bs-target="#leaveTypeModal">
-                Add</button>
+                {{ customCreateSelect('leaveType', 'leaveType', '', '', ['sick' => 'Sick', 'paid' => 'Paid']) }}
+                <button class="btn btn-outline-light" type="button" data-bs-toggle="modal"
+                    data-bs-target="#leaveTypeModal">
+                    Add</button>
             </div>
 
-    </div>
-    <div class="col-lg-4">{{ createDate('leaveStart', 'leaveStart', 'Leave Start Date') }}
-    </div>
-    <div class="col-lg-4">{{ createDate('leaveEnd', 'leaveEnd', 'Leave End Date') }}
-    </div>
-    <div class="col-lg-12">
+        </div>
+        <div class="col-lg-4">{{ createDate('leaveStart', 'leaveStart', 'Leave Start Date') }}
+        </div>
+        <div class="col-lg-4">{{ createDate('leaveEnd', 'leaveEnd', 'Leave End Date') }}
+        </div>
+        <div class="col-lg-12">
 
-        {{ createLabel('remarks', 'form-label col-form-label', 'Remarks') }}
-        {{ createTextArea('remarks', 'remarks', 'Remarks', '', '') }}
-    </div> <br>
-    <div class="col-md-12"><?php createButton('btn-primary btn-store', '', 'Submit'); ?>
-    </div>
+            {{ createLabel('remarks', 'form-label col-form-label', 'Remarks') }}
+            {{ createTextArea('remarks', 'remarks', 'Remarks', '', '') }}
+        </div> <br>
+        <div class="col-md-12"><?php createButton('btn-primary btn-store', '', 'Submit'); ?>
+        </div>
 
 </form>
-
-
