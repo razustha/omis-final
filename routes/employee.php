2248a9a6506f5205b20e182afc36000780bbfecb
pilla20202020 <bsbishalpilla@gmail.com>
2023-02-04 15:47:09 +0545

    employee leave done

--- routes/employee.php
+++ routes/employee.php
@@ -44,3 +44,5 @@ Route::get('/dashboard', function () {
     $employees = Employee::where('status', '<>', -1)->orderBy('created_at', 'desc')->get();
     return view('omis\welcome', compact('employees'));
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
