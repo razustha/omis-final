db831433ac7d25422714e9b6509f54564ea250e0
razustha <razustha@hotmail.com>
2023-02-06 08:51:57 +0545

    edit on project blade

--- resources/views/omis/project/workfromhome/create.blade.php
+++ resources/views/omis/project/workfromhome/create.blade.php
@@ -1,41 +1,62 @@
 @extends('omis.partials.layouts')
-        @section('content')
-        <div class="nk-content">
-            <div class="container">
-                <div class="nk-content-inner">
+@section('content')
+    <div class="nk-content">
+        <div class="container">
+            <div class="nk-content-inner">
                 <div class="nk-content-body">
-                <div class="nk-block-head">
-                <div class="nk-block-head-between flex-wrap gap g-2">
-                    <div class="nk-block-head-content">
-                        <h2 class="nk-block-title">Add Workfromhome</h1>
+                    <div class="nk-block-head">
+                        <div class="nk-block-head-between flex-wrap gap g-2">
+                            <div class="nk-block-head-content">
+                                <h2 class="nk-block-title">Add Workfromhome</h1>
 
+                            </div>
+                            <div class="nk-block-head-content">
+                                <ul class="d-flex">
+                                    <li>
+                                        <a href="{{ route('project.workfromhome.index') }}"
+                                            class="btn btn-md d-md-none btn-primary">
+                                            <em class="icon ni ni-plus"></em>
+                                            <span>View Cities</span>
+                                        </a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div class="nk-block-head-content">
-                    <ul class="d-flex"> <li>
-                        <a href="{{ route('project.workfromhome.index') }}" class="btn btn-md d-md-none btn-primary">
-                                <em class="icon ni ni-plus"></em>
-                                <span>View Cities</span>
-                            </a>
-                        </li>
 
-                    </ul>
-                </div>
-                </div>
-            </div>
-
-            <div class="nk-block">
+                    <div class="nk-block">
 
                         <div class="card">
                             <div class="card-body">
-                <form method="POST" action="{{route('project.workfromhome.store')}}" enctype="multipart/form-data">
- @csrf 
-<div class="row"><div class="col-lg-6">{{createText("programName","programName","ProgramName")}}
-</div><div class="col-lg-6">{{createText("duration","duration","Duration")}}
-</div><div class="col-lg-6">{{createText("date","date","Date")}}
-</div><div class="col-lg-6">{{createText("time","time","Time")}}
-</div><div class="col-lg-6">{{createText("alias","alias","Alias")}}
-</div><div class="col-lg-6">{{customCreateSelect("status","status",'',"Status",['1'=>'Active','0'=>'Inactive'])}}
-</div><div class="col-lg-6">{{createText("remarks","remarks","Remarks")}}
-</div> <br> <div class="col-md-12"><?php createButton("btn-primary","","Submit"); ?>
-</div> </form></div></div></div></div></div></div></div></div>
+                                <form method="POST" action="{{ route('project.workfromhome.store') }}"
+                                    enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-lg-6">{{ createText('programName', 'programName', 'ProgramName') }}
+                                        </div>
+                                        <div class="col-lg-6">{{ createText('duration', 'duration', 'Duration') }}
+                                        </div>
+                                        <div class="col-lg-6">{{ createText('date', 'date', 'Date') }}
+                                        </div>
+                                        <div class="col-lg-6">{{ createText('time', 'time', 'Time') }}
+                                        </div>
+                                        <div class="col-lg-6">{{ createText('alias', 'alias', 'Alias') }}
+                                        </div>
+                                        <div class="col-lg-6">
+                                            {{ customCreateSelect('status', 'status', '', 'Status', ['1' => 'Active', '0' => 'Inactive']) }}
+                                        </div>
+                                        <div class="col-lg-6">{{ createText('remarks', 'remarks', 'Remarks') }}
+                                        </div> <br>
+                                        <div class="col-md-12"><?php createButton('btn-primary', '', 'Submit'); ?>
+                                        </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 @endsection
--- resources/views/omis/work/workProjects/ajax/create.blade.php
+++ resources/views/omis/work/workProjects/ajax/create.blade.php
@@ -3,37 +3,40 @@
     <div class="row">
         <div class="col-lg-6">{{ createText('projectTitle', 'projectTitle', 'Project Title') }}
         </div>
+        <div class="col-lg-6">{!! getSelectForForeignColumn('tbl_department', 'department_id', 'departmentName', '','','Department') !!}
+        </div>
         {{-- <div class="col-lg-6">{{ createText('projectStartClient', 'projectStartClient', 'Project Client') }}
         </div> --}}
-        <div class="col-lg-6">{{ createDate('projectStartDate', 'projectStartDate', 'Project Start Date') }}
+        <div class="col-lg-6">
+            
+                {{ createLabel('Assigned Employee', 'form-label col-form-label', 'Assigned Employee') }}
+                <select name="employee_id[]" class="form-control employee_id" multiple>
+
+                </select>
+            
+            
         </div>
-        <div class="col-lg-6">{{ createDate('projectEndDate', 'projectEndDate', 'Project End Date') }}
+        <div class="col-lg-3">{{ createDate('projectStartDate', 'projectStartDate', 'Start Date') }}
+        </div>
+        <div class="col-lg-3">{{ createDate('projectEndDate', 'projectEndDate', 'End Date') }}
         </div>
         <div class="col-lg-6">{{ createText('projectPriority', 'projectPriority', 'Project Priority') }}
         </div>
         {{-- <div class="col-lg-6">{{ createText('companyName_id', 'companyName_id', 'Company Id') }}
         </div> --}}
 
-        <div class="col-lg-6">{!! getSelectForForeignColumn('tbl_department', 'department_id', 'departmentName', '') !!}
-        </div>
+      
 
 
 
         <div class="col-lg-6">{{ customCreateSelect('status', 'status', '', 'Status', ['1' => 'Active', '0' => 'Inactive']) }}
         </div>
+      
         <div class="col-lg-12">
-            <div class="col-lg-12">
-                {{ createLabel('Assigned Employee', 'form-label col-form-label', 'Assigned Employee') }}
-                <select name="employee_id[]" class="form-control employee_id" multiple>
-
-                </select>
-            </div>
-        </div>
-        <div class="col-lg-6">
             {{ createLabel('projectDescription', 'for-label col-form-label', 'Description') }}{{ createTextArea('projectDescription', 'projectDescription', 'ProjectDescription', '', '') }}
 
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             {{ createLabel('remarks', 'form-label col-form-label', 'Remarks') }}{{ createTextArea('remarks', 'remarks', 'Remarks', '', '') }}
         </div> <br>
         <div class="col-md-12"><?php createButton('btn-primary btn-store', '', 'Submit'); ?>
