4d338e01b84b4c0671833a2f8ab13eab5166da2f
razustha <razustha@hotmail.com>
2023-02-06 17:44:05 +0545

    gitco

--- app/Models/Hr/Shiftroster.php
+++ app/Models/Hr/Shiftroster.php
@@ -1,48 +1,41 @@
 <?php
-        namespace App\Models\Hr;
 
-        use App\Models\User;
-        use Illuminate\Database\Eloquent\Casts\Attribute;
-        use Illuminate\Database\Eloquent\Factories\HasFactory;
-        use Illuminate\Database\Eloquent\Model;
-        use App\Traits\CreatedUpdatedBy;
+namespace App\Models\Hr;
 
-        class Shiftroster extends Model
-        {
-            use HasFactory, CreatedUpdatedBy;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\CreatedUpdatedBy;
 
-            protected $table = 'tbl_shiftroster';
-            protected $primaryKey = 'shiftRoster_id';
-            public $timestamps = true;
-            protected $fillable =[
-                'rosterName_id',
-'shiftFromDate',
-'shiftToDate',
-'shift',
-'createdOn',
-'createdBy',
-'alias',
-'status',
-'remarks',
-'created_at',
-'updated_at',
-'updatedBy',
+class Shiftroster extends Model
+{
+    use HasFactory, CreatedUpdatedBy;
 
-            ];
-            protected $appends = ['status_name','shift_name'];
-            // 
-            // protected function getShiftNameAttribute()
-            // {
-               
-            // return $this->shift== 0 ? '<span> Morning </span>':'<span> Day </span>';
-            // }
-            // 
-            
+    protected $table = 'tbl_shiftroster';
+    protected $primaryKey = 'shiftRoster_id';
+    public $timestamps = true;
+    protected $fillable = [
+        'rosterName_id',
+        'shiftFromDate',
+        'shiftToDate',
+        'shift',
+        'createdOn',
+        'createdBy',
+        'alias',
+        'status',
+        'remarks',
+        'created_at',
+        'updated_at',
+        'updatedBy',
 
-            protected function getStatusNameAttribute()
-            {
-                return $this->status == 1 ? '<span class="badge text-bg-success-soft"> Active </span>' : '<span class="badge text-bg-danger-soft">Inactive</span>';
-            }
+    ];
+    protected $appends = ['status_name', 'shift_name'];
+
+    protected function getStatusNameAttribute()
+    {
+        return $this->status == 1 ? '<span class="badge text-bg-success-soft"> Active </span>' : '<span class="badge text-bg-danger-soft">Inactive</span>';
+    }
 
     protected function createdBy(): Attribute
     {
@@ -57,4 +50,4 @@
             get: fn ($value) => User::find($value) ? User::find($value)->name : '',
         );
     }
-        }
+}
